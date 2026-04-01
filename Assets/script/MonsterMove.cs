b69b6c77e6db66688275c0dc5b4be8138b8c4509
AxIFSxA <75491111h@gmail.com>
2026-04-01 15:47:21 +0900

    4_01 unity

--- Assets/script/MonsterMove.cs
+++ Assets/script/MonsterMove.cs
@@ -6,29 +6,39 @@ public class MonsterMove : MonoBehaviour
 {
     public float spd = 3.0f;
 
-    public GameObject target;
+    GameObject target;
     Vector3 dircet = Vector3.down;
+
+    public GameObject prefabsExplosion;
+
+    int rndNum;
+
+
     // Start is called before the first frame update
     private void Start()
     {
-        int rndNum = Random.Range(0, 10);
+        target = GameObject.Find("Chracter");
+        rndNum = Random.Range(0, 10);
 
-        if(rndNum % 3 ==0)
-        {
-            
-            dircet = target.transform.position-transform.position;
-            dircet.Normalize(); 
-        }
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (rndNum % 3 == 0)
+        {
+
+            dircet = target.transform.position - transform.position;
+            dircet.Normalize();
+        }
         transform.position = transform.position+dircet*spd*Time.deltaTime;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        GameObject explosionOBJ = Instantiate(prefabsExplosion);
+        explosionOBJ.transform.position = transform.position;   
+
         Destroy(collision.gameObject);
 
         Destroy(gameObject);
